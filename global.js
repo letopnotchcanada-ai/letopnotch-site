9e6544717c0f9c70d808d58dc1aae6562adff363
letopnotchcanada-ai <letopnotchcanada@gmail.com>
2026-04-15 15:28:53 -0400

    Update global.js

--- global.js
+++ global.js
@@ -21,12 +21,6 @@
   window.gtag = gtag;
 })();
 
-// ── MICROSOFT CLARITY ──
-(function(c,l,a,r,i,t,y){
-  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "wbqbtjde93");
 
 // ── META PIXEL (FACEBOOK) ──
 !function(f,b,e,v,n,t,s)
